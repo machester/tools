78823108b2adf2ab72a295b5d9a08e9386d9f8a4
machester <shusheng1991@gmail.com>
2019-06-26 18:40:21 +0800

    update build ota diff package

--- rk3288_android7.1_shell_tools/build_ota_diff_package.c
+++ rk3288_android7.1_shell_tools/build_ota_diff_package.c
@@ -3,103 +3,97 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include <limits.h>
-#include <linux/input.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <inttypes.h>
 #include <errno.h>
-#include <hardware/sensors.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <dirent.h>
 #include <math.h>
 #include <poll.h>
 #include <pthread.h>
-#include <linux/input.h>
+// for folder access
 #include <dirent.h>
+// acces file change time
+#include <utime.h>
+// get file priproty
+#include <sys/stat.h>
+
+
 
 #define usr_debug(fmt)											\
 				do {											\
 						printf("<BUILD OTA DIFF PATCH>" fmt "\n"); 	\
 				} while(0)
+                
+                
+// struct utimbuf {
+//     time_t actime;
+//     time_t modtime;
+// };
+
+struct stat file_stat;
+                
 
 void format(void)
 {
 	usr_debug("-------------------------------------------------------------------");
 	usr_debug("./build_ota_diff_package.sh <ota_package_new> <ota_package_old>");
-	sr_debug("--------------------------------------------------------------------");
+	usr_debug("--------------------------------------------------------------------");
 }
 
-int find_dir(const char * name)
+
+int find_dir(char * target_path, char * dir_name)
 {
-struct dirent **name_list;
-int n = scandir(path,&name_list,0,alphasort);
-	if(n < 0)
-	{ 
-	 	printf( "scandir return %d \n",n);
-	}
-	else
-	{
-		int index=0;
-		while(index < n)
-    	{
-   			printf("name: %s\n", name_list[index]->d_name);
-    		free(name_list[index++]);
-    	}
-    	free(name_list);
+    int index, numbers;
+    struct dirent **dir_list;
+    DIR *dir = opendir(target_path);
+    if(NULL == dir) {
+        perror("target path cannot access");
+        return -1;
     }
+    // move in target path
+    chdir(target_path);
+    // get dir list
+    numbers = scandir(".",&dir_list, NULL, NULL);
+	if(numbers < 0)
+	{ 
+	 	perror("scandir");
+         exit(EXIT_FAILURE);
+	}
+    for(index = 0; index < numbers; index++) {
+        printf("|\n");
+        printf("|");
+        printf("--- %s\n", dir_list[index]->d_name);
+    }
+    // -------------------------------------------------------------------------------
+    // find folder
+    for(index = 0; index < numbers; index++) {
+        if(!strcmp(dir_list[index]->d_name, dir_name)) {
+            printf("target folder found in dir_list[%d]->d_name: %s\n", index, dir_list[index]->d_name);
+        }
+    }
+    return 0;
 }
 
-int maint(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if(2 != argc) {
-		format();
-		exit(1);
-	}
-
+	// if(2 != argc) {
+	// 	format();
+	// 	exit(1);
+	// }
+    printf("current path: ");
+    system("pwd");
+    find_dir(".", "xfm10213_driver");
 
 
 
 
 	return 0;
 }
-
-
-#!/bin/bash
-####################################################################################
-#./build/tools/releasetools/ota_from_target_files -v -i OTA_PACKAGE/rk3288-target_files-eng.old_jiangq.zip -p out/host/linux-x86 -k build/target/product/security/testkey OTA_PACKAGE/rk3288-target_files-eng.new_jiangq.zip  OTA_PACKAGE/rk3288-target_files-ota_diff-eng.zip
-#
-#####################################################################################
-CUR_PATH=`PWM`
-BUILD_PROP_PATH=$CUR_PATH/out/target/product/rk3288/system
-function format()
-{
-	echo "-----------------------------------------------------------"
-	echo "	./build_ota_diff_package.sh <ota_package_new> <ota_package_old>"
-	echo "-----------------------------------------------------------"
-}
-
-echo "---------------- start build ota diff package ------------------------"
-if [ $# -neq 2 ]; then
-	format
-echo "current folder dir: $CUR_PATH"
-
-# mkimage.sh
-echo "make and copy android images"
-./mkimage.sh
-if [ $# -neq 0 ]; then
-    echo "Make image ok!"
-else
-    echo "Make image failed!"
-    exit 1
-fi
-
-build.prop
-
-echo "------------------------------ end -----------------------------------"
