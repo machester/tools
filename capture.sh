d165e57278076a98163e2d4178e370f24e1487bf
machester <shusheng1991@gmail.com>
2019-11-14 17:21:45 +0800

    modifiy capture.sh picture storge place

--- capture.sh
+++ capture.sh
@@ -2,12 +2,14 @@
 echo "----------- start caputre --------------"
 index=0
 rm -f storage/emulated/0/Pictures/*
-while [ $index -lt 1000 ]
+while [ $index -lt 20000 ]
 do
-	DATE=$(date  +%Y%m%d-%H%M%S)
-	adb shell screencap -p storage/emulated/0/Pictures/cap_$index_$DATE.png
-	let index++;
+	DATE=$(date  +%Y%m%d_%H%M%S)
+	# adb shell screencap -p storage/emulated/0/Pictures/cap_$index_$DATE.png
 	echo "---> index = $index"
+	echo "caputre file name pic_$index-$DATE.png"
+	adb shell screencap -p storage/emulated/0/Screenshots/pic_$index-$DATE.png
+	let index++;
 	sleep 1s
 	
 done
