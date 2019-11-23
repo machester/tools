b433aaa2ec9661140fb795e664ab138b27c551ee
machester <shusheng1991@gmail.com>
2019-11-23 18:33:56 +0800

    update tools

---
+++ sublime_config/sublime_config.js
@@ -0,0 +1,11 @@
+{
+	"color_scheme": "Packages/Theme - itg.flat/itg.dark.tmTheme",
+	"font_size": 10,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"itg_sidebar_tree_xsmall": true,
+	"itg_small_tabs": true,
+	"theme": "itg.flat.dark.sublime-theme"
+}
