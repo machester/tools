68f6691c9809dacec9a1ae5d10ac852ecf720c99
machester <shusheng1991@gmail.com>
2019-12-17 13:56:07 +0800

    add manual stop for bsh repo_sync.sh

--- repo_sync/repo_sync.sh
+++ repo_sync/repo_sync.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+trap 'onCtrlC' INT
+function onCtrlC () {
+    echo "manual stopped"
+    exit
+}
+
 echo "-------------- start sync --------------"
 repo sync -c --no-clone-bundle
 while [ $? -ne 0 ]; 
 do  
-    repo sync -c --no-clone-bundle
+    repo sync -c --no-clone-bundle -j8
 done
 echo "------------- sync finished -------------"
