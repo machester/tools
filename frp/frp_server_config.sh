efdaeb42176c1c9b83e13ba8f30bd603da93f680
machester <machester000@icloud.com>
2022-07-08 00:05:03 +0800

    update: frp config bash

--- frp/frp_server_config.sh
+++ frp/frp_server_config.sh
@@ -7,9 +7,19 @@
 
 #!/bin/bash
 
+FRP_DIR=$(pwd)
+echo "---> cur dir: $FRP_DIR"
+
+FRP_TOOLS_DIR=$FRP_DIR/frp_tools
+
+
 echo "download frp..."
-bash ../frp_tools/frp_download.sh
+bash $FRP_DIR/frp_download.sh
 if [ $? -ne 0 ]; then
     echo "download failed"
 fi
 
+cp -vf $FRP_DIR/frp_v0.43.0_linux_amd64.tar.gz .
+tar -xvf frp_v0.43.0_linux_amd64.tar.gz
+
+cd frp_0.43.0_linux_amd64
